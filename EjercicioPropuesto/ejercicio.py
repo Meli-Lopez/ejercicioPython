d5b749f4889eeced269a9390605e7e55df430633
Meli-Lopez <129072033+Meli-Lopez@users.noreply.github.com>
2023-11-24 11:30:04 -0500

    Update ejercicio.py

--- EjercicioPropuesto/ejercicio.py
+++ EjercicioPropuesto/ejercicio.py
@@ -1,43 +1,52 @@
+class EquipoComputo:
+    def __init__(self, idEquipo, cargador, mouse, ambiente):
+        self.ID = idEquipo
+        self.Cargador = cargador
+        self.Mouse = mouse
+        self.Ambiente = ambiente
+        self.Novedades = []
+
 # Se define un diccionario vacio llamado 'equipos' para almacenar la informacion de los equipos de computo.
 equipos = {}
 
 # Funcion para agregar un equipo al diccionario 'equipos'
 def agregarEquipo(idEquipo, cargador, mouse, ambiente):
-    equipos[idEquipo] = {'ID': idEquipo, 'Cargador': cargador, 'Mouse': mouse, 'Ambiente': ambiente, 'Novedades': []}
+    equipos[idEquipo] = EquipoComputo(idEquipo, cargador, mouse, ambiente)
+    print("El equipo a sido registrado correctamente")
 
 # Funcion para agregar una novedad a un equipo especifico.
 def agregarNovedad(idEquipo, fecha, descripcion):
     if idEquipo in equipos:
-        equipos[idEquipo]['Novedades'].append({'Fecha': fecha, 'Descripcion': descripcion})
-        print("La novedad ha sido agregada con éxito al equipo con ID {}.".format(idEquipo))
+        equipos[idEquipo].Novedades.append({'Fecha': fecha, 'Descripcion': descripcion})
+        print("La novedad ha sido agregada con exito al equipo con ID {}.".format(idEquipo))
     else:
         print("El equipo con ID {} no existe.".format(idEquipo))
 
 # Funcion para obtener un reporte de equipos con novedades.
 def reporteNovedades():
-    equiposConNovedades = [equipo for equipo in equipos.values() if equipo['Novedades']]
+    equiposConNovedades = [equipo for equipo in equipos.values() if equipo.Novedades]
     return equiposConNovedades
 
 # Funcion para mostrar todos los equipos y sus novedades.
 def mostrarEquipos():
     for equipo in equipos.values():
-        print("ID: {}".format(equipo['ID']))
-        print("Cargador: {}".format(equipo['Cargador']))
-        print("Mouse: {}".format(equipo['Mouse']))
-        print("Ambiente: {}".format(equipo['Ambiente']))
+        print("ID: {}".format(equipo.ID))
+        print("Cargador: {}".format(equipo.Cargador))
+        print("Mouse: {}".format(equipo.Mouse))
+        print("Ambiente: {}".format(equipo.Ambiente))
         print("Novedades:")
-        for novedad in equipo['Novedades']:
+        for novedad in equipo.Novedades:
             print("- Fecha: {}, Descripcion: {}".format(novedad['Fecha'], novedad['Descripcion']))
         print("-------------")
 
 # Funcion para modificar la informacion de un equipo existente.
 def modificarEquipo(idEquipo, cargador, mouse, ambiente):
     if idEquipo in equipos:
-        equipos[idEquipo]['Cargador'] = cargador
-        equipos[idEquipo]['Mouse'] = mouse
-        equipos[idEquipo]['Ambiente'] = ambiente
+        equipos[idEquipo].Cargador = cargador
+        equipos[idEquipo].Mouse = mouse
+        equipos[idEquipo].Ambiente = ambiente
     else:
-        print("El equipo con ID {} no existe.".format(idEquipo)) #El .format se utiliza para insertar valores en posiciones especificas
+        print("El equipo con ID {} no existe.".format(idEquipo))
 
 # Funcion para eliminar un equipo del diccionario 'equipos'.
 def eliminarEquipo(idEquipo):
@@ -67,23 +76,23 @@ def menu():
             idEquipo = input("Ingrese el ID del equipo que desea agregar: ")
             cargador = input("El equipo cuenta con cargador? (si/no): ")
             mouse = input("El equipo cuenta con mouse? (si/no): ")
-            ambiente = input("En qué ambiente se encuentra el equipo?: ")
+            ambiente = input("En que ambiente se encuentra el equipo?: ")
             agregarEquipo(idEquipo, cargador, mouse, ambiente)
         elif opcion.lower() == 'b':
             # Se solicita informacion de la novedad y se llama a la funcion 'agregarNovedad'.
             idEquipo = input("Ingrese el ID del equipo al que le va a registrar una novedad: ")
             fecha = input("Ingrese la fecha de la novedad: ")
-            descripcion = input("Ingrese la descripción de la novedad: ")
+            descripcion = input("Ingrese la descripcion de la novedad: ")
             agregarNovedad(idEquipo, fecha, descripcion)
         elif opcion.lower() == 'c':
             # Se busca un equipo por ID y se muestra su informacion si existe.
             idEquipo = input("Ingrese el ID del equipo que desea buscar: ")
             if idEquipo in equipos:
                 print("Datos del equipo:")
-                print("ID: {}".format(equipos[idEquipo]['ID']))
-                print("Cargador: {}".format(equipos[idEquipo]['Cargador']))
-                print("Mouse: {}".format(equipos[idEquipo]['Mouse']))
-                print("Ambiente: {}".format(equipos[idEquipo]['Ambiente']))
+                print("ID: {}".format(equipos[idEquipo].ID))
+                print("Cargador: {}".format(equipos[idEquipo].Cargador))
+                print("Mouse: {}".format(equipos[idEquipo].Mouse))
+                print("Ambiente: {}".format(equipos[idEquipo].Ambiente))
             else:
                 print("El equipo no se encuentra en la base de datos")
         elif opcion.lower() == 'd':
@@ -93,14 +102,14 @@ def menu():
                 print("No hay ninguna novedad en los equipos")
             else:
                 for equipo in equiposNovedades:
-                    print("ID: {}".format(equipo['ID']))
-                    for novedad in equipo['Novedades']:
+                    print("ID: {}".format(equipo.ID))
+                    for novedad in equipo.Novedades:
                         print("- Fecha: {}, Descripcion: {}".format(novedad['Fecha'], novedad['Descripcion']))
         elif opcion.lower() == 'e':
             # Se llama a la funcion para mostrar todos los equipos.
             mostrarEquipos()
         elif opcion.lower() == 'f':
-            # Se solicita la informacion para modificar un equipo y se llama a la funcion correspondiente.
+        # Se solicita la informacion para modificar un equipo y se llama
             idEquipo = input("Ingrese el ID del equipo que desea modificar: ")
             cargador = input("El equipo cuenta con cargador? (si/no): ")
             mouse = input("El equipo cuenta con mouse? (si/no): ")
@@ -115,9 +124,9 @@ def menu():
             break
         else:
             # Se informa al usuario si la opcion ingresada no es valida.
-            print("La opción que ingresó no es válida, verifique las opciones e ingrese una correcta")
+            print("La opcion que ingreso no es valida, verifique las opciones e ingrese una correcta")
 
-    print("El programa ha terminado con éxito :D.")
+    print("El programa ha terminado con exito :D.")
 
 # Se llama a la funcion principal 'menu' para ejecutar el programa.
 menu()
